47c18f3e8c1da02bd0edcacd353f8664fd2c7855
Jack <775893055@qq.com>
2018-09-20 18:45:29 +0800

    three

---
+++ test/MoneyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use JackChow\WeChat\Money;
+
+class MoneyTest extends PHPUnit_Framework_TestCase
+{
+    public function testCanBeNegated()
+    {
+        $a = new Money(1);
+
+        $b = $a->negate();
+
+        $this->assertEquals(-1, $b->getAmount());
+    }
+}
+
+
